a1a3ebf85905d73e66ccc312619457cfa194e5d1
Willfred Azamo <wilfred.azamo@gmail.com>
2026-02-27 13:26:31 +0100

    - Upgrade package dependencies to latest versions in `Libota.Application` and `Libota.Desktop` projects. - Refactor `AuthorizationCheckInterceptor` for improved null safety and logging. - Replace `ItemsRepeater` with `ListBox` and enhance keyboard navigation in `MembersList.axaml`. - Adjust markup formatting and properties for consistent code style. - Change `Shell` inheritance to use `AppWindow` from `Huskui.Avalonia`. - Include missing `DependentUpon` entries in `csproj` files. - Minor UI improvements and styling changes (`ListBoxItem` padding, focusable elements, separators).

--- Libota.Application/Security/Authorization/AuthorizationCheckInterceptor.cs
+++ Libota.Application/Security/Authorization/AuthorizationCheckInterceptor.cs
@@ -6,20 +6,28 @@ using Microsoft.Extensions.Logging;
 
 namespace Libota.Application.Security.Authorization;
 
-public class AuthorizationCheckInterceptor(ILogger<AuthorizationCheckInterceptor> logger, IUserSessionService userSessionService) : IInterceptor
+public class AuthorizationCheckInterceptor(
+    ILogger<AuthorizationCheckInterceptor> logger,
+    IUserSessionService userSessionService) : IInterceptor
 {
     public void Intercept(IInvocation invocation)
     {
-        if (!invocation.MethodInvocationTarget.GetCustomAttributes<AuthorizeAttribute>().Any())
+        if (!(invocation.MethodInvocationTarget ?? throw new InvalidOperationException())
+            .GetCustomAttributes<AuthorizeAttribute>().Any())
+        {
             invocation.Proceed();
+        }
         else
         {
             var currentUserName = userSessionService.CurrentUserSession?.UserName;
-            if (currentUserName != "admin" )
+            if (currentUserName != "admin")
             {
-                throw new SecurityException($"unauthorized operation [{currentUserName} => {invocation.TargetType}.{invocation.Method.Name}]");
+                throw new SecurityException(
+                    $"unauthorized operation [{currentUserName} => {invocation.TargetType}.{invocation.Method.Name}]");
             }
-            logger.LogInformation($"checking authorization before calling {invocation.TargetType}.{invocation.Method.Name}");
+
+            logger.LogInformation(
+                "checking authorization before calling {InvocationTargetType}.{MethodName}", invocation.TargetType, invocation.Method.Name);
             invocation.Proceed();
         }
     }
