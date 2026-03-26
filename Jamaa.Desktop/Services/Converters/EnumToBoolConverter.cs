1fb8998e5832991d7edb38144e64a8214227ec8b
Njilefac <wilfred.azamo@gmail.com>
2026-03-26 23:53:10 +0100

    50-memberlist-view-mode-switching (#51)
    * implement display mode switching for member list
    
    Signed-off-by: Willfred Azamo <wilfred.azamo@gmail.com>
    
    * Refactor `MembersList.axaml` to simplify `ToggleButton` markup and consolidate styling in `JamaaStyles.axaml`.
    
    Signed-off-by: Willfred Azamo <wilfred.azamo@gmail.com>
    
    * Refactor and enhance UI styling in `MembersList.axaml` and `JamaaStyles.axaml` for consistency and improved usability. Adjust layout and introduce custom `Avatar` control with updated styles.
    
    Signed-off-by: Willfred Azamo <wilfred.azamo@gmail.com>
    
    ---------
    
    Signed-off-by: Willfred Azamo <wilfred.azamo@gmail.com>

---
+++ Jamaa.Desktop/Services/Converters/EnumToBoolConverter.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Jamaa.Desktop.Services.Converters;
+
+public class EnumToBoolConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value == null || parameter == null)
+            return false;
+
+        return value.Equals(parameter);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is true && parameter != null)
+        {
+            return parameter;
+        }
+
+        return null;
+    }
+}
