1ad365b63d132a07f9413bd97aa045844dcd5b51
Wilfred Azamo <wilfred.azamo@gmail.com>
2026-03-31 02:56:10 +0200

    Add `IncrementConverter` to format `SelectedIndex` in `MemberCarousel` and update resources.

---
+++ Jamaa.Desktop/Services/Converters/IncrementConverter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Jamaa.Desktop.Services.Converters;
+
+public class IncrementConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int intValue)
+        {
+            return intValue + 1;
+        }
+
+        if (value is long longValue)
+        {
+            return longValue + 1;
+        }
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int intValue)
+        {
+            return intValue - 1;
+        }
+
+        if (value is long longValue)
+        {
+            return longValue - 1;
+        }
+
+        return value;
+    }
+}
