65891300f0b0fd11f8b697475fec7b01a97ce540
Wilfred Azamo <wilfred.azamo@gmail.com>
2026-03-31 23:26:02 +0200

    Refactor `MembersGrid`: Implement `DataGridEnterBehavior` for improved enter-key handling, replacing inline logic with reusable behavior.

---
+++ Jamaa.Desktop/Shared/Behaviors/DataGridEnterBehavior.cs
@@ -0,0 +1,72 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Interactivity;
+
+namespace Jamaa.Desktop.Shared.Behaviors;
+
+public class DataGridEnterBehavior : Behavior<Control>
+{
+    public static readonly StyledProperty<ICommand?> CommandProperty =
+        AvaloniaProperty.Register<DataGridEnterBehavior, ICommand?>(nameof(Command));
+
+    public ICommand? Command
+    {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public static readonly StyledProperty<object?> CommandParameterProperty =
+        AvaloniaProperty.Register<DataGridEnterBehavior, object?>(nameof(CommandParameter));
+
+    public object? CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        if (AssociatedObject != null)
+        {
+            AssociatedObject.AddHandler(InputElement.KeyDownEvent, OnKeyDown, RoutingStrategies.Tunnel);
+        }
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        if (AssociatedObject != null)
+        {
+            AssociatedObject.RemoveHandler(InputElement.KeyDownEvent, OnKeyDown);
+        }
+    }
+
+    private void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter && Command != null)
+        {
+            var parameter = CommandParameter;
+            if (parameter == null)
+            {
+                if (AssociatedObject is DataGrid dg)
+                {
+                    parameter = dg.SelectedItem;
+                }
+                else
+                {
+                    parameter = AssociatedObject?.DataContext;
+                }
+            }
+
+            if (Command.CanExecute(parameter))
+            {
+                Command.Execute(parameter);
+                e.Handled = true;
+            }
+        }
+    }
+}
