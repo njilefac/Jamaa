fb4706ee622d36fe2fbd1ecbbff8020748a591cd
Wilfred Azamo <wilfred.azamo@gmail.com>
2026-03-31 23:51:59 +0200

    Refactor `MemberCard`: Remove unused namespaces and methods to streamline code.

--- Jamaa.Desktop/Members/Components/MemberCard.axaml.cs
+++ Jamaa.Desktop/Members/Components/MemberCard.axaml.cs
@@ -1,14 +1,6 @@
-using System;
-using System.Linq;
-using System.Reactive;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Input;
 using Avalonia.Markup.Xaml;
-using Avalonia.VisualTree;
-using Huskui.Avalonia.Controls;
-using Jamaa.Desktop.Members.ViewModels;
-using Jamaa.Desktop.Members.Messages;
 
 namespace Jamaa.Desktop.Members.Components;
 
@@ -22,8 +14,6 @@ public partial class MemberCard : UserControl
         get => GetValue(IsSelectedProperty);
         set => SetValue(IsSelectedProperty, value);
     }
-
-    private MemberListViewModel? GetViewModel() => this.FindAncestorOfType<MembersList>()?.DataContext as MemberListViewModel;
 
     public MemberCard()
     {
